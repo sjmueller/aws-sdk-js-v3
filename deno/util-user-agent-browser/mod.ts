f758de7fd2c4c751a0faddd06584aaace7808db6
Christoph Gysin <christoph.gysin@gmail.com>
2020-08-11 12:56:05 +0300

    Remove getting user-agent from navigator

--- deno/util-user-agent-browser/mod.ts
+++ deno/util-user-agent-browser/mod.ts
@@ -4,9 +4,5 @@ export function defaultUserAgent(
   packageName: string,
   packageVersion: string
 ): string {
-  let originUserAgent =
-    typeof navigator !== "undefined" && typeof navigator.userAgent === "string"
-      ? navigator.userAgent
-      : "";
-  return `aws-sdk-js-v3-${packageName}/${packageVersion} ${originUserAgent}`;
+  return `aws-sdk-js-v3-${packageName}/${packageVersion}`;
 }
