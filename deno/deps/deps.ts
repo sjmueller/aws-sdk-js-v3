238029175224790816970410ad3e98c0f8f9fbfd
Christoph Gysin <christoph.gysin@gmail.com>
2020-11-25 09:27:36 +0200

    Remove unused deno-deps

--- deno/deps/deps.ts
+++
@@ -1,6 +0,0 @@
-export * as uuid from "https://deno.land/std@0.68.0/uuid/mod.ts";
-export * as fs from "https://deno.land/std@0.68.0/node/fs.ts";
-export * as os from "https://deno.land/std@0.68.0/node/os.ts";
-export * as path from "https://deno.land/std@0.68.0/node/path.ts";
-
-
