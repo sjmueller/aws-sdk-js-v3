f527b182717a4a8f6c930c6f8d7a7ad3693662ce
Christoph Gysin <christoph.gysin@gmail.com>
2020-12-07 10:56:11 +0200

    feat(deno): add deno packages

---
+++ packages/credential-provider-imds-deno/remoteProvider/httpRequest.ts
@@ -0,0 +1,21 @@
+import { Buffer } from "https://deno.land/std@0.79.0/node/buffer.ts";
+import { ProviderError } from "../../property-provider/mod.ts";
+type RequestOptions = any;
+
+/**
+ * @internal
+ */
+export async function httpRequest(options: RequestOptions): Promise<Buffer> {
+  try {
+    const response = await fetch({ method: "GET", ...options });
+
+    const { status: statusCode = 400 } = response;
+    if (statusCode < 200 || 300 <= statusCode) {
+      throw Object.assign(new ProviderError("Error response received from instance metadata service"), { statusCode });
+    }
+
+    return Buffer.from(await response.text());
+  } catch (err) {
+    throw Object.assign(new ProviderError("Unable to connect to instance metadata service"), err);
+  }
+}
