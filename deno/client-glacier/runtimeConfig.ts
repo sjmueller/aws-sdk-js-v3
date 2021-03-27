438618c2a48e4b18f8828b3cf63c291ada44a206
Christoph Gysin <christoph.gysin@gmail.com>
2021-03-27 09:58:18 +0200

    feat(deno): generate clients

--- deno/client-glacier/runtimeConfig.ts
+++ deno/client-glacier/runtimeConfig.ts
@@ -1,4 +1,4 @@
-const packageInfo = { version: "3.8.0" };
+const packageInfo = { version: "3.8.1" };
 
 import { bodyChecksumGenerator } from "../body-checksum-browser/mod.ts";
 import { NODE_REGION_CONFIG_FILE_OPTIONS, NODE_REGION_CONFIG_OPTIONS } from "../config-resolver/mod.ts";
